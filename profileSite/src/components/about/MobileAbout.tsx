367e42f3fc2e9757b1e5d722b66735167d2a588a
augutis44 <kraujalytee@gmail.com>
2023-09-05 18:14:13 +0300

    footer all done

--- profileSite/src/components/about/MobileAbout.tsx
+++ profileSite/src/components/about/MobileAbout.tsx
@@ -8,7 +8,7 @@ const MobileAbout = ({ icons }) => {
             </div>
             <div className='flex flex-col'>
                 {icons.map((icon, i) => (
-                    <div className="bg-light-pink rounded-xl mt-3 mx-3 p-4 drop-shadow">
+                    <div key={i} className="bg-light-pink rounded-xl mt-3 mx-3 p-4 drop-shadow">
                         <div className="flex justify-center my-2 items-center">
                             <icon.name className="fill-red text-3xl mr-2" />
                             <div className="text-red text-3xl font-primary text-center">
--- profileSite/src/components/footer/Footer.tsx
+++ profileSite/src/components/footer/Footer.tsx
@@ -1,27 +1,24 @@
 import { HiOutlineMail } from "react-icons/hi";
-import { AiOutlinePhone } from "react-icons/ai";
 import { FiGithub } from "react-icons/fi";
 import { FaLinkedin } from "react-icons/fa";
 
-
-
 const Footer = () => {
     return (
-        <div className="bg-light-pink flex flex-col md:flex-row justify-between	py-10 md:px-20">
-            <div className="text-red text-5xl md:text-6xl text-center mb-8 font-primary uppercase drop-shadow">
+        <div className="bg-light-pink flex flex-col lg:flex-row justify-around py-10 items-center">
+            <div className="text-red text-4xl md:text-6xl text-center mb-8 font-primary uppercase drop-shadow">
                 got questions?
             </div>
             <div className="flex flex-col">
-                <div className="flex flex-row items-center text-xl md:text-3xl drop-shadow">
-                    <HiOutlineMail className="text-red text-5xl mx-3"/>
+                <div className="flex flex-row items-center text-xl md:text-2xl drop-shadow">
+                    <HiOutlineMail className="text-red text-4xl mr-3" />
                     kraujalytee@gmail.com
                 </div>
-                <div className="flex flex-row items-center text-xl md:text-3xl drop-shadow">
-                    <FiGithub className="text-red text-5xl mx-3" />
+                <div className="flex flex-row items-center text-xl md:text-2xl drop-shadow break-all">
+                    <FiGithub className="text-red text-4xl mr-3 shrink-0" />
                     <a href="https://github.com/augutis44">https://github.com/augutis44</a>
                 </div>
-                <div className="flex flex-row items-center text-xl md:text-3xl drop-shadow">
-                    <FaLinkedin className="text-red text-5xl mx-3" />
+                <div className="flex flex-row items-center text-xl md:text-2xl drop-shadow break-all">
+                    <FaLinkedin className="text-red text-4xl mr-3 shrink-0" />
                     <a href="https://www.linkedin.com/in/augustina-kraujalyte/">https://www.linkedin.com/in/augustina-kraujalyte/</a>
                 </div>
             </div>
