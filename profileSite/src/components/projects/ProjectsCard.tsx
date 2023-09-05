09fd8a4e4980495e05cafb09286e92ac85a0dd2f
augutis44 <kraujalytee@gmail.com>
2023-09-05 22:54:45 +0300

    finnished projects and technologies

--- profileSite/src/components/projects/ProjectsCard.tsx
+++ profileSite/src/components/projects/ProjectsCard.tsx
@@ -19,12 +19,12 @@ const ProjectsCard = () => {
     return (
         <div className="flex flex-col md:flex-row">
             {projects.map((project, i) => (
-                <div key={i} className="flex items-center flex-col text-center basis-1/3 mx-5 mt-10 lg:mt-0">
-                    <PiStarFourFill className='md:text-6xl text-3xl text-pink drop-shadow' />
-                    <div className="text-red text-2xl md:text-3xl font-seondary my-4 drop-shadow">
+                <div key={i} className="flex items-center flex-col text-center basis-1/3 md:mx-5 mt-6 md:mt-10 lg:mt-0">
+                    <PiStarFourFill className='md:text-4xl text-2xl text-pink drop-shadow' />
+                    <div className="text-red text-2xl md:text-3xl font-seondary my-2 md:my-4 drop-shadow">
                         {project.name}
                     </div>
-                    <div className="md:text-xl font-secondary mb-5">
+                    <div className="text-base md:text-xl font-secondary mb-2 md:mb-5">
                         {project.about}
                     </div>
                     <div className="h-48 w-36 lg:h-20r lg:w-15r bg-red rounded-xl drop-shadow-lg">
