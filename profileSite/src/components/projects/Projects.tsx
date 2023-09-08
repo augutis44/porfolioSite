3ff02384ba5c94fb39e9148fe04aacaa9087bf2c
augutis44 <kraujalytee@gmail.com>
2023-09-09 00:33:12 +0300

    final

--- profileSite/src/components/projects/Projects.tsx
+++ profileSite/src/components/projects/Projects.tsx
@@ -2,8 +2,8 @@ import ProjectsCard from "./ProjectsCard";
 
 const Projects = () => {
     return (
-        <div className="bg-light-pink flex flex-col justify-center lg:h-screen py-10 px-5 md:p-10">
-            <div className="text-red text-3xl md:text-6xl font-primary uppercase text-center drop-shadow">
+        <div className="bg-light-pink flex flex-col justify-center lg:h-screen py-10 px-5 md:px-10">
+            <div className="text-red text-3xl md:text-6xl font-primary uppercase text-center drop-shadow mb-6 md:mb-10">
                 My projects
             </div>
             <div>
