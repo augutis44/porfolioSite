d3026e2e0145d6adb0532aee831dcb6950efdd07
augutis44 <kraujalytee@gmail.com>
2024-02-09 01:29:13 +0200

    footer changes

--- src/components/footer/Footer.tsx
+++ src/components/footer/Footer.tsx
@@ -4,22 +4,30 @@ import { FaLinkedin } from "react-icons/fa";
 
 const Footer = () => {
     return (
-        <div className="bg-light-pink flex flex-col lg:flex-row justify-around py-10 items-center">
-            <div className="text-red text-4xl md:text-6xl text-center mb-8 font-primary uppercase drop-shadow">
-                got questions?
+        <div>
+            <div className="bg-pink">
+                <div className="bg-[url('/footerWave.png')] bg-[length:100%] h-[5rem] md:h-[7rem] xl:h-[9rem] 2xl:h-[11rem] w-full bg-left-bottom bg-no-repeat">
+
+                </div>
             </div>
-            <div className="flex flex-col font-secondary text-xl md:text-2xl break-all">
-                <div className="flex flex-row items-center drop-shadow">
-                    <HiOutlineMail className="text-red text-4xl mr-3" />
-                    kraujalytee@gmail.com
+
+            <div className="bg-red flex flex-col lg:flex-row justify-around p-10 items-center">
+                <div className="text-light-pink text-4xl md:text-6xl text-center mb-8 font-primary uppercase drop-shadow">
+                    got questions?
                 </div>
-                <div className="flex flex-row items-center drop-shadow">
-                    <FiGithub className="text-red text-4xl mr-3 shrink-0" />
-                    <a href="https://github.com/augutis44">https://github.com/augutis44</a>
-                </div>
-                <div className="flex flex-row items-center drop-shadow">
-                    <FaLinkedin className="text-red text-4xl mr-3 shrink-0" />
-                    <a href="https://www.linkedin.com/in/augustina-kraujalyte/">https://www.linkedin.com/in/augustina-kraujalyte/</a>
+                <div className="flex flex-col font-secondary text-xl md:text-2xl break-all">
+                    <div className="flex flex-row items-center drop-shadow">
+                        <HiOutlineMail className="text-light-pink text-4xl mr-3" />
+                        <p className="text-light-pink">kraujalytee@gmail.com</p>
+                    </div>
+                    <div className="flex flex-row items-center drop-shadow">
+                        <FiGithub className="text-light-pink text-4xl mr-3 shrink-0" />
+                        <a href="https://github.com/augutis44" className="text-light-pink">https://github.com/augutis44</a>
+                    </div>
+                    <div className="flex flex-row items-center drop-shadow">
+                        <FaLinkedin className="text-light-pink text-4xl mr-3 shrink-0" />
+                        <a href="https://www.linkedin.com/in/augustina-kraujalyte/" className="text-light-pink">https://www.linkedin.com/in/augustina-kraujalyte/</a>
+                    </div>
                 </div>
             </div>
         </div>
